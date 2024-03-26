68de1b0735a4d5a392e32a06195ac5c6783ae384
Serhii Hrushevitskiy <ipz203_gsv@student.ztu.edu.ua>
2024-03-26 19:36:53 +0200

    Частина друга

--- screens/Gallery.js
+++ screens/Gallery.js
@@ -1,4 +1,5 @@
-import { StyleSheet, Image, ScrollView } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Image, ScrollView, View, Dimensions } from 'react-native';
 
 const styles = StyleSheet.create({
     containerGallery: {
@@ -6,25 +7,47 @@ const styles = StyleSheet.create({
         flexDirection: 'row',
         flexWrap: 'wrap',
         justifyContent: 'space-evenly',
-        marginBottom:50,
-      },
-      imageGalery: {
+        marginBottom: 50,
+    },
+    imageGalery: {
         marginTop: 10,
         margin: 5,
-        width: '20%',
+        width: Dimensions.get('window').width / 3 - 10, // розрахунок ширини для відображення по три в рядку
         aspectRatio: 1,
-      },
-    });
+    },
+});
 
 const Gallery = () => {
-    const numImage = 40; 
-  
+    const [images, setImages] = useState([]);
+
+    useEffect(() => {
+        fetch('https://raw.githubusercontent.com/SerhiiHrushevitskiy/MobileLabs/master/data/gallery.json')
+            .then(response => response.json())
+            .then(data => setImages(data))
+            .catch(error => console.error('Помилка завантаження даних:', error));
+    }, []);
+
+    // Функція для розділення масиву на групи по три
+    const chunkArray = (array, chunkSize) => {
+        const chunkedArr = [];
+        for (let i = 0; i < array.length; i += chunkSize) {
+            chunkedArr.push(array.slice(i, i + chunkSize));
+        }
+        return chunkedArr;
+    };
+
     return (
-      <ScrollView contentContainerStyle={styles.containerGallery}>
-      {[...Array(numImage)].map((_, index) => (
-        <Image style={styles.imageGalery} source={require('../assets/gallery.png')}  />
-      ))}
-      </ScrollView>
+        <ScrollView contentContainerStyle={styles.containerGallery}>
+            {/* Розділити масив на групи по три та вивести */}
+            {chunkArray(images, 3).map((row, rowIndex) => (
+                <View key={rowIndex} style={{ flexDirection: 'row' }}>
+                    {row.map(item => (
+                        <Image key={item.id} style={styles.imageGalery} source={{ uri: item.image }} />
+                    ))}
+                </View>
+            ))}
+        </ScrollView>
     );
-  }
-  export default Gallery;
+}
+
+export default Gallery;
--- screens/Home.js
+++ screens/Home.js
@@ -1,61 +1,67 @@
-import { StyleSheet, Text, View, Image, ScrollView} from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, Text, View, ScrollView, Image } from 'react-native';
 
 const styles = StyleSheet.create({
-    container: {
-      backgroundColor: 'lightblue',
-      alignItems: 'center',
-      justifyContent: 'flex-start',
-    },
-    newsContainer: {
-        flexDirection: 'row',
-        padding: 10,
-        borderBottomWidth: 1,
-        borderBottomColor: '#ccc',
-        marginBottom: 10,
-      },
-      newsImage: {
-        width: 80,
-        height: 80,
-        marginRight: 10,
-        borderRadius: 5,
-      },
-      newsTextContainer: {
-        flex: 1,
-      },
-      newsTitle: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginBottom: 5,
-      },
-      newsDate: {
-        fontSize: 14,
-        color: '#888',
-        marginBottom: 5,
-      },
-      newsExcerpt: {
-        fontSize: 16,
-      },
-    });
+  container: {
+    backgroundColor: 'lightblue',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  newsContainer: {
+    flexDirection: 'row',
+    padding: 10,
+    borderBottomWidth: 1,
+    borderBottomColor: '#ccc',
+    marginBottom: 10,
+  },
+  newsImage: {
+    width: 80,
+    height: 80,
+    marginRight: 10,
+    borderRadius: 5,
+  },
+  newsTextContainer: {
+    flex: 1,
+  },
+  newsTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+  },
+  newsDate: {
+    fontSize: 14,
+    color: '#888',
+    marginBottom: 5,
+  },
+  newsExcerpt: {
+    fontSize: 16,
+  },
+});
 
 const Home = () => {
-    const numNews = 10; 
+  const [news, setNews] = useState([]);
+
+  useEffect(() => {
+    fetch('https://raw.githubusercontent.com/SerhiiHrushevitskiy/MobileLabs/master/data/home.json')
+      .then(response => response.json())
+      .then(data => setNews(data))
+      .catch(error => console.error('Помилка завантаження даних:', error));
+  }, []);
 
   return (
     <ScrollView contentContainerStyle={styles.container}>
-        {[...Array(numNews)].map((_, index) => (
-          <View key={index} style={styles.newsContainer}>
-            <Image
-              style={styles.newsImage}
-              source={require('../assets/gallery.png')} 
-            />
-            <View style={styles.newsTextContainer}>
-              <Text style={styles.newsTitle}>Новина {index + 1}</Text>
-              <Text style={styles.newsDate}>Дата новини</Text>
-              <Text style={styles.newsExcerpt}>Короткий текст новини...</Text>
-            </View>
+      {news.map(item => (
+        <View key={item.id} style={styles.newsContainer}>
+          <Image style={styles.newsImage} source={{ uri: item.image }} />
+          <View style={styles.newsTextContainer}>
+            <Text style={styles.newsTitle}>{item.title}</Text>
+            <Text style={styles.newsDate}>{item.date}</Text>
+            <Text numberOfLines={2} style={styles.newsExcerpt}>{item.excerpt}</Text>
           </View>
-        ))}
+        </View>
+      ))}
     </ScrollView>
   );
 }
+
 export default Home;
